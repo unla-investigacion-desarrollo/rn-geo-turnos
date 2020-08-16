4336755cd8466be884b6c56679957dcfa373bfa3
Gonzalo Berro <gonzaloberro@Gonzas-Macbook-Pro.local>
2020-08-16 17:51:12 -0300

    Arreglo error mapa login

--- components/Login/MarkUbicacion.jsx
+++ components/Login/MarkUbicacion.jsx
@@ -16,15 +16,6 @@ export default function MarkUbicacion(props) {
         latitudeDelta: 0.003,
         longitudeDelta: 0.003,
       }}
-    >
-      {negocio.latitude !== null && negocio.longitude !== null ? ( //Solo pongo el marcador si tengo una ubicacion
-        <MapView.Marker
-          coordinate={{
-            latitude: negocio.latitude,
-            longitude: negocio.longitude,
-          }}
-        ></MapView.Marker>
-      ) : null}
-    </MapView>
+    ></MapView>
   );
 }
