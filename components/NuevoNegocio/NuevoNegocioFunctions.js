91643081181fbee3d57e34a425166d9acadd8279
ltrucco <56170216+ltrucco@users.noreply.github.com>
2020-11-15 20:52:10 -0300

    Cambio api geo

--- components/NuevoNegocio/NuevoNegocioFunctions.js
+++ components/NuevoNegocio/NuevoNegocioFunctions.js
@@ -5,17 +5,20 @@ export const searchPosition = async (direccion) => {
   //Funcion que va a consultar al endpoint de google maps para obtener la informacion de la direccion que se introducio
 
   if (direccion !== "") {
-    const url = "https://maps.googleapis.com/maps/api/geocode/json?address="; //Url del endpoint
-    const key = api_key; //Apikey que necesita google maps, CAMBIAR PARA PASAR A PRODUCCION
+    // const url = "https://maps.googleapis.com/maps/api/geocode/json?address="; //Url del endpoint
+    // const key = api_key; //Apikey que necesita google maps, CAMBIAR PARA PASAR A PRODUCCION
 
-    let address = direccion.replace(/\s/g, "+"); //Reemplazo los espacios por +
-    address = address + "+argentina"; //Concateno argentina a la busqueda
+    // let address = direccion.replace(/\s/g, "+"); //Reemplazo los espacios por +
+    // address = address + "+argentina"; //Concateno argentina a la busqueda
 
-    let key_url = "&key=" + key; //concateno la key para armar la url
+    // let key_url = "&key=" + key; //concateno la key para armar la url
+    const url = "https://api.opencagedata.com/geocode/v1/json?q="
+    const address = encodeURI(direccion)
+    const key = "&key=9d2aa62241c640a6bc93e2fe34cae046&language=es&pretty=1"
 
     // console.log(url + address + key_url); //armo la url completa para hacer el fetch
 
-    const responseJson = await axios.get(url + address + key_url);
+    const responseJson = await axios.get(url+address+key);
 
     const data = responseJson.data;
     let localizacionNegocio = {
@@ -25,9 +28,10 @@ export const searchPosition = async (direccion) => {
 
     if (data.results.length > 0) {
       localizacionNegocio = {
-        longitude: data.results[0].geometry.location.lng,
-        latitude: data.results[0].geometry.location.lat,
+        longitude: data.results[0].bounds.northeast.lng,
+        latitude: data.results[0].bounds.northeast.lat,
       };
+      console.log(localizacionNegocio)
     }
 
     return localizacionNegocio;
